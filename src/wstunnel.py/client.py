136b7965d3b94b921eceb0d03fd4c3774427e096
ironbeep567 <177458325+ironbeep567@users.noreply.github.com>
2024-08-16 10:48:04 +0000

    Argument parsing

--- src/wstunnel.py/client.py
+++ src/wstunnel.py/client.py
@@ -1,4 +1,4 @@
-import asyncio, logging
+import asyncio, logging, argparse
 import websockets
 from base import async_copy, wrap_stream_writer
 
@@ -16,19 +16,45 @@ async def conn_handler(reader, writer, uri, token):
         await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
     logger.info(f"Connection {peer_addr!r} closed")
 
-async def main(listen, uri, token):
+async def main(args):
     async def handler(reader, writer):
-        return await conn_handler(reader, writer, uri, token)
-    server = await asyncio.start_server(handler, listen[0], listen[1])
+        return await conn_handler(reader, writer, args.uri, args.token)
+    server = await asyncio.start_server(handler, args.listen[1], args.listen[2])
     async with server:
-        logger.info(f"Listening on {listen!r}")
+        logger.info(f"Listening on {args.listen!r}")
         await server.serve_forever()
 
 if __name__ == "__main__":
-    listen = ("127.0.0.1", 8080)
-    uri = "ws://127.0.0.1:9090"
-    token = "test"
-    logLevel = logging.INFO
+    def parse_listen(listen):
+        proto,rest = listen.split(":", 1)
+        ip,port = rest.rsplit(":", 1)
+        return proto,ip,int(port)
+    def validate_ws_uri(uri):
+        if not uri.startswith("wss://") and not uri.startswith("ws://"):
+            raise ValueError(f"Not a valid websocket uri: {uri}")
+    parser = argparse.ArgumentParser(prog="Wstunnel client")
+    parser.add_argument("--uri", required=True,
+                        metavar="ws[s]://HOST:PORT", help="Server URI")
+    parser.add_argument("--token", "-t")
+    parser.add_argument("--listen", "-l", default="tcp:127.0.0.1:8080",
+                        metavar="tcp:IP:PORT", help="Listen address")
+    parser.add_argument("--log-level", default="info", choices=["debug", "info", "warning", "error", "critical"])
+    args = parser.parse_args()
+    if args.log_level == "debug":
+        logLevel = logging.DEBUG
+    elif args.log_level == "warning":
+        logLevel = logging.WARNING
+    elif args.log_level == "error":
+        logLevel = logging.ERROR
+    elif args.log_level == "critical":
+        logLevel = logging.CRITICAL
+    else:
+        logLevel = logging.INFO
     logging.basicConfig(level=logLevel)
-    asyncio.run(main(listen, uri, token))
+    args.listen = parse_listen(args.listen)
+    validate_ws_uri(args.uri)
+    if args.token is not None and args.uri.startswith("ws://"):
+        logger.warning("Sending token over insecure connection")
+    print(f"Listening on {args.listen[1]}:{args.listen[2]}")
+    asyncio.run(main(args))
 
--- src/wstunnel.py/server.py
+++ src/wstunnel.py/server.py
@@ -1,4 +1,4 @@
-import asyncio, logging, hmac, functools
+import asyncio, logging, hmac, functools, argparse
 import websockets, websockets.server
 from base import async_copy, wrap_stream_writer
 
@@ -10,14 +10,14 @@ class WebSocketServerProtocol(websockets.server.WebSocketServerProtocol):
         self.token = token
     
     async def process_request(self, path, request_headers):
-        if not hmac.compare_digest(token, request_headers.get("x-token", "")):
+        if not hmac.compare_digest(self.token, request_headers.get("x-token", "")):
             logger.info(f"Connection {self.remote_address!r} auth failed")
             return 404, [], b""
         return await super().process_request(path, request_headers)
 
 async def ws_handler(ws, backend):
     logger.info(f"Connection {ws.remote_address!r} accepted")
-    reader,writer = await asyncio.open_connection(backend[0], backend[1])
+    reader,writer = await asyncio.open_connection(backend[1], backend[2])
     f_write, f_close_writer = wrap_stream_writer(writer)
     tasks = [asyncio.create_task(async_copy(ws.recv, f_write,
                                             f_close_writer, False)),
@@ -26,21 +26,45 @@ async def ws_handler(ws, backend):
     await asyncio.wait(tasks, return_when=asyncio.ALL_COMPLETED)
     logger.info(f"Connection {ws.remote_address!r} closed")
 
-async def main(listen, backend, token):
+async def main(args):
     async def handler(ws):
-        return await ws_handler(ws, backend)
+        return await ws_handler(ws, args.backend)
     class ServerProtocol(WebSocketServerProtocol):
-        __init__ = functools.partialmethod(WebSocketServerProtocol.__init__, token=token)
-    async with websockets.serve(handler, listen[0], listen[1],
+        __init__ = functools.partialmethod(WebSocketServerProtocol.__init__, token=args.token)
+    async with websockets.serve(handler, args.listen[0], args.listen[1],
                                 create_protocol=ServerProtocol,
                                 server_header=""):
         await asyncio.Future() # Serve forever
 
 if __name__ == "__main__":
-    listen = ("127.0.0.1", 9090)
-    backend = ("127.0.0.1", 9091)
-    token = "test"
-    logLevel = logging.INFO
+    def parse_listen(listen):
+        ip,port = listen.split(":")
+        return ip,int(port)
+    def parse_backend(backend):
+        proto,rest = backend.split(":", 1)
+        ip,port = rest.rsplit(":", 1)
+        return proto,ip,int(port)
+    parser = argparse.ArgumentParser(prog="Wstunnel server")
+    parser.add_argument("--listen", "-l", required=True,
+                        metavar="IP:PORT", help="Listen address")
+    parser.add_argument("--backend", "-b", required=True,
+                        metavar="tcp:IP:PORT", help="Backend address")
+    parser.add_argument("--token", "-t")
+    parser.add_argument("--log-level", default="info", choices=["debug", "info", "warning", "error", "critical"])
+    args = parser.parse_args()
+    if args.log_level == "debug":
+        logLevel = logging.DEBUG
+    elif args.log_level == "warning":
+        logLevel = logging.WARNING
+    elif args.log_level == "error":
+        logLevel = logging.ERROR
+    elif args.log_level == "critical":
+        logLevel = logging.CRITICAL
+    else:
+        logLevel = logging.INFO
     logging.basicConfig(level=logLevel)
-    asyncio.run(main(listen, backend, token))
+    args.listen = parse_listen(args.listen)
+    args.backend = parse_backend(args.backend)
+    print(f"Listening on ws://{args.listen[0]}:{args.listen[1]}")
+    asyncio.run(main(args))
 
